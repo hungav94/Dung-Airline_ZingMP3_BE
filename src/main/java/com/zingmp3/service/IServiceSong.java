d881ad31d62ff7b2b70edb642cff14a8df72937a
cong <congphamhuu1994@gmail.com>
2019-12-23 18:02:20 +0700

    update ControllerSong

--- src/main/java/com/zingmp3/service/IServiceSong.java
+++ src/main/java/com/zingmp3/service/IServiceSong.java
@@ -1,4 +1,11 @@
 package com.zingmp3.service;
 
+import com.zingmp3.model.Song;
+
 public interface IServiceSong {
+    Iterable<Song> findAll();
+
+    void save(Song song);
+
+    void delete(long id);
 }
--- src/main/java/com/zingmp3/service/ServiceSong.java
+++ src/main/java/com/zingmp3/service/ServiceSong.java
@@ -1,4 +1,26 @@
 package com.zingmp3.service;
 
-public class ServiceSong {
+import com.zingmp3.model.Song;
+import com.zingmp3.repository.IRepositorySong;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceSong implements IServiceSong{
+    @Autowired
+    private IRepositorySong repositorySong;
+    @Override
+    public Iterable<Song> findAll() {
+        return repositorySong.findAll();
+    }
+
+    @Override
+    public void save(Song song) {
+        repositorySong.save(song);
+    }
+
+    @Override
+    public void delete(long id) {
+        repositorySong.deleteById(id);
+    }
 }
