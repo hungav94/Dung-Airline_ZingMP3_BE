d6e045eca6c489253bc9daf07db88f33ef9b6053
hungavcodegym <anhung94@gmail.com>
2020-01-08 19:23:50 +0700

    fix token

---
+++ src/main/java/com/zingmp3/model/jwt/JwtRequest.java
@@ -0,0 +1,17 @@
+package com.zingmp3.model.jwt;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class JwtRequest implements Serializable {
+    private static final long serialVersionUID = 5926468583005150707L;
+
+    private String username;
+    private String password;
+
+    public JwtRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
--- src/main/java/com/zingmp3/repository/IRepositorySong.java
+++ src/main/java/com/zingmp3/repository/IRepositorySong.java
@@ -2,6 +2,7 @@ package com.zingmp3.repository;
 
 import com.zingmp3.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
